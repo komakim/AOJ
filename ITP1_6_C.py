d908745acd56f693992b2443a98e956d4ff1116d
Komakim <komaki.masako.kf9@is.naist.jp>
2017-02-05 12:14:08 +0900

    ITP1_6_C.py

--- ITP1_6_C.py
+++ ITP1_6_C.py
@@ -3,17 +3,22 @@ def reviewRe():
     #print ' '.join(map(str,map(str,residence[i][j])))
     for area in range(4):
         for floor in range(3):
-            print area ,floor
-            print ''.join(map(str,residence[area][floor][:]))
-        print "###################"
+            sys.stdout.write(' ')
+            print ' '.join(map(str,residence[area][floor][:]))
+        if area <= 2:
+            print "####################"
+
+def move_in():
+    n=input()
+    for i in range(n):
+        b,f,r,v = map(int,raw_input().split())
+        adding(b,f,r,v)
+
+def adding(b,f,r,v):
+    residence[b-1][f-1][r-1] += v
 
 
 if __name__ == '__main__':
     residence = [[[0 for room in range(10)] for floor in range(3)] for area in range(4)]
-    print residence
+    move_in()
     reviewRe()
-    #n = input()
-    #for i in range(n):
-    #    b,f,r,v = map(int,raw_input().split())
-    #for i in range(area) :
-    #    residence [i]
